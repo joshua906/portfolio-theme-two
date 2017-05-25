aaeb558bf0125a87567c4e582262a9598eeb2e22
Joshua Matthews <joshuamatthews906@yahoo.com>
2017-05-25 18:21:19 -0500

    services page updated

---
+++ page-services.php
@@ -0,0 +1,83 @@
+<?php get_header(); ?>
+	
+	<!-- Lets work together -->
+	
+		<div class="matthews-divider-topics caption">
+			<div class="container-post">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="matthews-headline">Lets work together</h1>
+						<hr class="text-left">
+						<p>Are you an ambitious Instructor or service business owner looking to increase your course signups and subscribers to grow your business?
+
+						<p>Are you a consultant looking to expand</p>
+
+						<p>Do you feel like your current website has been holding your business back, but you are unsure of where to start to fix it?</p>
+	
+						<p>If you answered yes to any of the questions above then I can help. Through my step by step process we will identify your business goals and how we can translate that to produce a profitable website that increases your sales, course signups, and subscribers. </p>
+
+						<p>Are you ready to grow your Business?</p>
+						
+						<a href="<?php echo get_page_link(6); ?>"><button>Im Ready!</button></a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="spacing"></div>
+			
+			<div class="matthews-service-image-padding">
+			<div class="featured-top-blog">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/matthews-service-image.jpg" alt="Matthews Service image">
+			</div>
+			</div>
+		</div>
+		
+	<!-- my process -->
+	<div class="my-process caption">
+		<div class="container-post">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="matthews-headline">My process</h1>
+					<hr class="text-left">
+					
+					<h3 class="blue-heading">1. Strategy & Research</h3>
+					<p>During our strategy session we will identify your business goals and what needs to be achieved to have a successful project. Then, through research and exploration we identify who your audience is and how we can develop a strategy that will reach and convert them.</p>
+					
+					<h3 class="blue-heading">2. Wireframe, Prototype & UI/UX Design</h3>
+					<p>I recognize that you need a beautiful site to reach your audience. But my design decisions aren't based off of solely what looks good or what I like the most. Rather, design should be used to guide the user through your website and call them to action. By combining both strategy and design, I will create an intuitive experience that accomplishes your business goals. </p>
+					
+					<h3 class="blue-heading">3. Development, Launch & Iteration</h3>
+					<p>Your new site has now been developed and launched (Woohoo!), but our work isn't over yet. Your site is a living organism and should be treated as such. To ensure your projects future success, I will analyze, and revise anything that may be causing friction within your site and keeping your customers from converting.</p>
+					
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container-post">
+	<hr>
+	</div>
+		
+		
+		
+		
+		
+		
+	<!-- Call to action -->
+	<div class="caption">
+	   <div class="container-post">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1 class="matthews-headline">Ready to get your project started?</h1>
+					<a href="<?php echo get_page_link(6); ?>"><button>YES!</button></a>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+	
+
+
+
+<?php get_footer(); ?>
